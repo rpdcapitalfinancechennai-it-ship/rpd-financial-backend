19b14508a0923e3bed8cf8010c60c7baacdb8e29
rpdcapitalfinancechennai-it-ship <rpdcapitalfinanceoffice@gmail.com>
2026-02-07 17:31:23 +0530

    Test

--- server.js
+++ server.js
@@ -27,30 +27,6 @@ app.use(bodyParser.json());
 
 // MongoDB connection
 const uri = process.env.MONGO_URI || "mongodb+srv://rpdcapitalfinanceoffice_db_user:rpddatabase@rpd.azqjbtb.mongodb.net/rpd-financial?retryWrites=true&w=majority&appName=rpd";
-
-console.log("GMAIL_USER:", process.env.GMAIL_USER);
-console.log("GMAIL_PASS:", process.env.GMAIL_APP_PASSWORD ? "LOADED" : "MISSING");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.sendgrid.net",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "apikey",
-    pass: process.env.SENDGRID_API_KEY
-  },
-});
-
-
-transporter.verify((err, success) => {
-  if (err) {
-    console.error("SMTP ERROR:", err);
-  } else {
-    console.log("SMTP READY ✅");
-  }
-});
-/*K964PKV8U6KW6QSQGENE28P9*/
-
 mongoose.connect(uri)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log("MongoDB connection error:", err));
@@ -62,8 +38,6 @@ app.use('/api/fd', require('./routes/fd'));
 app.use('/api/chit', require('./routes/chit'));
 app.use('/api/report', require('./routes/report'));
 app.use('/receipts', express.static(path.join(__dirname, 'receipts')));
-//app.use('/api/contact', require('./routes/home'));
-app.use('/api/contact', require('./routes/home')(transporter));
 
 //  Port setup for Render
 const PORT = process.env.PORT || 10000;
